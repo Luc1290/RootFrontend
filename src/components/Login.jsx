7c231e30ee0c5762983d0a650d7e478d04d3ac21
Luc <Lucparguel12720@gmail.com>
2025-03-31 17:06:26 +0200

    Login AuthO

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,16 @@
-// 📁 Login.jsx
 import styles from './Login.module.css';
 
 export default function Login() {
+  const backendUrl = "https://www.rootai.fr"; // Fixé en prod uniquement
+
   const handleLogin = () => {
-    window.location.href = "http://localhost:8080/api/auth/google-login"; // Remplace par https://www.rootai.fr en prod
+    window.location.href = `${backendUrl}/api/auth/google-login`;
   };
 
   return (
     <div className={styles.loginContainer}>
       <div className={styles.card}>
-        <h2>Connexion à Root:_</h2>
+        <h2>Se connecter</h2>
         <p>Connecte-toi avec ton compte Google pour commencer l’expérience.</p>
         <button onClick={handleLogin} className={styles.loginBtn}>
           Se connecter avec Google
